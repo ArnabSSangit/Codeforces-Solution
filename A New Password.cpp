307bb92874c2d71bb80f3c1ffdc6b3a191d7aab8
ArnabSSangit <63497768+ArnabSSangit@users.noreply.github.com>
2022-09-15 00:13:43 +0600

    Create A New Password.cpp

---
+++ A New Password.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<string>
+using namespace std;
+ 
+int main() {
+ 
+  string s="abcdefghijklmnopqrstuvwxyz";
+   string s2="";
+   int n,k;
+   cin>>n>>k;
+  for(int i=0;i<n;i++){
+  	s2+=s[i%k];
+  }
+ 
+ cout<<s2<<endl;
+ 
+ 
+ 
+ 
+ 
+ 
+ 	return 0;
